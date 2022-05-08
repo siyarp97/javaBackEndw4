ed96de88880a7885c1c1b2bb0680322bfc1700da
siyarp97 <102292361+siyarp97@users.noreply.github.com>
2022-05-09 01:07:40 +0300

    Us ödevi yüklendi

---
+++ usHesabiOdevi_w4.java
@@ -0,0 +1,24 @@
+package W4;
+import java.util.Scanner;
+public class usHesabiOdevi_w4 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a,b;
+
+        System.out.print("Ussu alinacak sayiyi giriniz : ");
+        a = input.nextInt();
+        System.out.print("Kac kere us alacaginizi giriniz : ");
+        b=input.nextInt();
+
+        int total=1;
+        for (int i =1; i<=b; i++){
+            total *= a;
+        }
+
+        System.out.print("Sonuc : " + total);
+
+    }
+
+}
